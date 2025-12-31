47909acdb126c026ac2559de7d5bf31be8470d52
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2026-01-01 02:32:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from telethon.tl.functions.payments import GetStarGiftsRequest
 import asyncio
 
 API_ID = 24526311        # <-- yahan apna API_ID
-API_HASH = "717d5df262e474f88d86c537a787c98d"  # <-- yahan apna API_HASH
-SESSION = "BQF2PecAeTUNNzoR0brOdxB_SQtDNxT-sOc1A1giDUQPANuGOoyNgNpQnnmnaWckd_MV4DPsdTAJFLtxsRcPBOuyt81hRaUX5J2ZVDLiA-Zb2ZwaoXkynFmNwzV6MHxEBEGU96aC91mGelD79eyXXTosavkb8EmzhGXkriF1hxGBcbI-cEfYQnNvkNyTr5vX-ZzDnE6lOsDzO_UnarAwqVpXyfdnNI5bdHf2blMQ2mCcVRS4wtFTBhYayhwEKFugYg3nWKdSbzDZ9x_AJ5ZVi_goMziq_flv2BWVg28pHOz4yVqBzxuluP7nc5etUX7TKwKFgu8rur7qJeLDV09iciAeZWxKEgAAAAHwVWW1AA"
+API_HASH = ""  # <-- yahan apna API_HASH
+SESSION = ""
 
 CHANNEL = "@fairyTailUpdates"
 
